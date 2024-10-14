6ac5ff107ffdffaf69a9ba14a0aace6aaa8e348f
nyxxy <gladilinnikita@yandex.ru>
2024-10-14 18:44:40 +0300

    реализовать основной функционал

--- stats/stats-server/src/main/java/ru/practicum/exception/ExceptionHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/exception/ExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class ExceptionHandler {
